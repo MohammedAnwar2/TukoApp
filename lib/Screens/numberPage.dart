92e4917a4b5926c4ee67efe37f25a0c298ec8582
mohammed anwar <moanbm123@gmail.com>
2023-10-23 02:50:24 +0530

    meesage

--- lib/Screens/numberPage.dart
+++ lib/Screens/numberPage.dart
@@ -1,3 +1,4 @@
+/*
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -64,3 +65,4 @@ class NumberPage extends StatelessWidget {
     );
   }
 }
+*/
